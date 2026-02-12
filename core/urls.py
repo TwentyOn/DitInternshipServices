61b868e820daa1206dfa36126a17778d30feb636
Alexey <aleksey-000@mail.ru>
2026-02-12 15:34:59 +0500

    конечные точки для получения документации openapi

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
     path("api/image_processing/", include("image_processing_api.urls")),
     path("api/statistics_pp/", include("statistics_pp.urls")),
     path("api/products_report_generator/", include("products_report_generator_api.urls")),
